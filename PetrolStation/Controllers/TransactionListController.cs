74ce32fc5c9ec4ec5cab8fbeea2954361dce6d25
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-11-26 02:56:25 +0100

    Creating transaction list view

---
+++ PetrolStation/Controllers/TransactionListController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PetrolStation.Models;
+using PetrolStation.Models.ModelePomocnicze;
+
+namespace PetrolStation.Controllers
+{
+    public class TransactionListController : Controller
+    {
+        private readonly ApplicationContext _context;
+        private List<ExtendedTransactionModel> extendedTransactions = new List<ExtendedTransactionModel>();
+
+        public TransactionListController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Show all transactions
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var transactions = _context.Transaction.ToList();
+            foreach(var t in transactions)
+            {
+                var actualExtendedTransaction = new ExtendedTransactionModel();
+                //assign transaction and client
+                actualExtendedTransaction.Transaction = t;
+                //assign loyality card
+                actualExtendedTransaction.LoyalityCard = _context.LoyalityCard.Where(x => x.IdLoyalityCard == t.IdLoyalityCard).FirstOrDefault();
+                //assign client
+                if(actualExtendedTransaction.LoyalityCard != null)
+                {
+                    actualExtendedTransaction.Client = _context.Client.Where(x => x == actualExtendedTransaction.LoyalityCard.Client).FirstOrDefault();
+                }
+                //assign car if there is an assigned client (if nie potrzebny chyba)
+                var car = _context.Car.Where(x => x.Client == actualExtendedTransaction.Client).FirstOrDefault();
+                actualExtendedTransaction.Car = car;
+                //assign fueling list
+                actualExtendedTransaction.FuelingList = _context.FuelingList.Where(x => x.Transaction == actualExtendedTransaction.Transaction).ToList();
+                foreach(var fuelingItem in actualExtendedTransaction.FuelingList)
+                {
+                    //?
+                    fuelingItem.Fueling = _context.Fueling.Where(x => x.IdFueling == fuelingItem.IdFueling).ToList()[0];
+                    //?
+                    fuelingItem.Fueling.Fuel = _context.Fuel.Where(x => x.IdFuel == fuelingItem.Fueling.IdFuel).ToList()[0];
+                }
+                //assign product list
+                actualExtendedTransaction.PurchasedProducts = _context.ProductList.Where(x => x.Transaction == actualExtendedTransaction.Transaction).ToList();
+                //dlaczego to konieczne?
+                foreach(var purchasedProduct in actualExtendedTransaction.PurchasedProducts)
+                {
+                    //assign product
+                    purchasedProduct.Product = _context.Product.Where(x => x.IdProduct == purchasedProduct.IdProduct).ToList()[0];
+                }
+                //Add extended transaction
+                extendedTransactions.Add(actualExtendedTransaction);
+            }
+            return View(extendedTransactions);
+        }
+    }
+}
---
+++ PetrolStation/Models/ModelePomocnicze/ExtendedTransactionModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetrolStation.Models.ModelePomocnicze
+{
+    public class ExtendedTransactionModel
+    {
+        public Transaction Transaction { get; set; }
+        public LoyalityCard LoyalityCard { get; set; }
+        public Client Client { get; set; }
+        public Car Car { get; set; }
+        public List<ProductList> PurchasedProducts { get; set; } = new List<ProductList>();
+        public List<FuelingList> FuelingList { get; set; } = new List<FuelingList>();
+
+    }
+}

19a1899b482724cab8881d35f390006689f280ee
Jakub Mroczkowski <jakub_mroczkowski@wp.pl>
2019-11-28 14:23:59 +0100

    Improved visuals

---
+++ PetrolStation/Controllers/SupplyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PetrolStation.Models;
+
+namespace PetrolStation.Controllers
+{
+    public class SupplyController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public SupplyController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Supply
+        public async Task<IActionResult> AddSupply()
+        {
+            ViewData["Produkty"] = await _context.Product.ToListAsync();
+            return View(await _context.Product.ToListAsync());
+        }
+    }
+}
--- PetrolStation/Models/ModelePomocnicze/TransactionModel.cs
+++ PetrolStation/Models/ModelePomocnicze/TransactionModel.cs
@@ -7,14 +7,13 @@ namespace PetrolStation.Models.ModelePomocnicze
 {
     public class TransactionModel
     {
-        public List<ProductQuantity> purchasedProducts { get; set; } = new List<ProductQuantity>();
+        public String boughtString { get; set; }
         public List<FuelingModel> purchasedFueling { get; set; } = new List<FuelingModel>();
-        public string NamePurchasedProduct { get; set; }
-        public int QuantityPurchasedProduct { get; set; }
         public decimal TransactionValue { get; set; }
         public bool IsInvoice { get; set; }
         public bool CardPayment { get; set; }
         public int IdLoyalityCard { get; set; }
         public Client client { get; set; } = new Client();
+        public Car clientCar { get; set; } = new Car();
     }
 }

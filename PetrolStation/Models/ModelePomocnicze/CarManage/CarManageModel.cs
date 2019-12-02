c0a56e9a7b48dd53f7e6f8cc2f3073cb747af2c9
kamilm758 <48297624+kamilm758@users.noreply.github.com>
2019-12-02 09:03:11 +0100

    added manage cars

---
+++ PetrolStation/Models/ModelePomocnicze/CarManage/CarManageModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetrolStation.Models.ModelePomocnicze.CarManage
+{
+    public class CarManageModel
+    {
+        public Client client { get; set; } = new Client();
+        public List<Car> cars { get; set; } = new List<Car>();
+
+        //field to add new car to a client
+        public Car carToAdd { get; set; } = new Car();
+    }
+}
